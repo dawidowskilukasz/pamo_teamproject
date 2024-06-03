02b639fb704e1a76e688462daa4f9a6516519b6f
Anna Rogala <v-annarogala@essencemediacom.com>
2024-06-03 21:16:22 +0200

    18 Add bottom navigation

--- app/src/main/java/com/example/workgood/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/workgood/ui/settings/SettingsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.workgood.ui.notifications
+package com.example.workgood.ui.settings
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.workgood.databinding.FragmentNotificationsBinding
+import com.example.workgood.databinding.FragmentSettingsBinding
 
-class NotificationsFragment : Fragment() {
+class SettingsFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentSettingsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-                ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val dashboardViewModel =
+                ViewModelProvider(this).get(SettingsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textSettings
+        dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
