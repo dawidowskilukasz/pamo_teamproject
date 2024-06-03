56e40bdea873044724f9c9f4582e53aaa61d6bfb
Anna Rogala <v-annarogala@essencemediacom.com>
2024-06-03 21:41:12 +0200

    2 Add Welcome Screen

--- app/src/main/java/com/example/workgood/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/workgood/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is Home Fragment"
+        value = "/Timer placeholder/"
     }
     val text: LiveData<String> = _text
 }
