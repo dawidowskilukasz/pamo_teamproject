22e2c6bcc60e78bae83f7a9b5e5e9fb1ae243859
lilaveth <blabladorrr@gmail.com>
2024-06-05 00:08:56 +0200

    7 Add saving a photo and confirmation dialog

--- app/src/main/java/com/example/workgood/MainActivity.kt
+++ app/src/main/java/com/example/workgood/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.workgood
 
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
