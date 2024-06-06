a2d4309cd524f48eb79c53dd3d01874a536aae21
dawidowskilukasz <s22621@pjwstk.edu.pl>
2024-06-06 23:00:54 +0200

    Delete unused import

--- app/src/main/java/com/example/workgood/MainActivity.kt
+++ app/src/main/java/com/example/workgood/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.workgood.activity
+package com.example.workgood
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,7 +7,6 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.workgood.R
 import com.example.workgood.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
