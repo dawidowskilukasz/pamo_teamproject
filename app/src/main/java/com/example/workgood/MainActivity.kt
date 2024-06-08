b0722bf51592e65c7a2aa0ba88a11b878fdcaee7
Anna <anna.rogala@gmail.com>
2024-06-08 21:06:26 +0200

    17 Remove the comment

--- app/src/main/java/com/example/workgood/MainActivity.kt
+++ app/src/main/java/com/example/workgood/MainActivity.kt
@@ -45,8 +45,6 @@ class MainActivity : AppCompatActivity() {
 
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
 
         val appBarConfiguration = AppBarConfiguration(
             setOf(
