625f8bd82d412228a30fdc6c28d95c1514d61012
Anna <anna.rogala@gmail.com>
2024-06-08 21:24:20 +0200

    17 Update documentation of HomeViewModel

--- app/src/main/java/com/example/workgood/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/workgood/ui/home/HomeViewModel.kt
@@ -9,10 +9,15 @@ import androidx.lifecycle.ViewModel
  * It manages the timer text that is observed by the fragment to update the UI when the timer text changes.
  */
 class HomeViewModel : ViewModel() {
-
+    
     private val _text = MutableLiveData<String>()
     val text: LiveData<String> = _text
 
+    /**
+     * Updates the timer text with the new text value.
+     *
+     * @param newText The new text value to update the timer text with.
+     */
     fun updateTimerText(newText: String) {
         _text.value = newText
     }
